3f085f010392ad62a17f70cf5ae2d33c22c50567
竹田悦人 <blademaster66@hotmail.com>
2016-02-12 15:07:49 +0900

    初版完成
    初版完成

---
+++ Test-AdministratorMode.ps1
@@ -0,0 +1,11 @@
+﻿<#
+    実行環境が管理者モードか検証します。
+#>
+function Test-AdministratorMode{
+    # ログインユーザーの情報を入手します。
+    $LogonUser = [System.Security.Principal.WindowsPrincipal]([System.Security.Principal.WindowsIdentity]::GetCurrent())
+
+    # ログオンユーザーにビルトインAdministratorのロールが割り当たっているか確認します。
+    # IsInRoleメソッドの戻り値はブールなので、そのまま戻り値として使用しています。
+    $LogonUser.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
+}
